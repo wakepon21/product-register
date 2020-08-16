8d913266d628ebd6a4a319368614763800810d32
wakepon21 <wkpn@outlook.jp>
2020-08-16 11:43:31 +0900

    test2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-
-  root  'users#index'  #この1行を追加
-  get "/" => 'wake-product-register#top'
-
-
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
